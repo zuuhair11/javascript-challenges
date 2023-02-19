da0265fcdbb3995ec30edaa77ab1beada30d027b
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2023-02-19 20:11:28 +0100

    Candy Sale

---
+++ 5/index.js
@@ -0,0 +1,35 @@
+import product from './data.js';
+
+
+/*
+   It's the day after Halloween 🎃 and all the candy is on sale!
+   
+   To buy up all the candy, use map() and filter() to put all the
+   candy into a `shoppingCart` array. 
+   
+   The new array should contain only the item and the price, like
+   this: 
+   
+   Expected output: 
+   [
+       {item: "🍭", price: 2.99},
+       {item: "🍫", price: 1.99}, 
+       {item: "🍬", price: 0.89}
+    ]
+*/
+
+function getSaleItems(data){
+    return data
+        .filter( item => item.type === 'sweet')
+        .map( item => {
+            return {
+                item: item.item,
+                price: item.price
+            }
+        });
+};
+
+
+const shoppingCart = getSaleItems(product);
+
+shoppingCart.forEach( item => console.log(item));
